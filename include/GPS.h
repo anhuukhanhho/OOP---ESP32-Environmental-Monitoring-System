d39f31c113847f44b4fc69c3d29c40795725701e
Hồ Hữu Khánh An <106583634+anhuukhanhho@users.noreply.github.com>
2026-01-10 23:56:58 +0700

    Update GPS.h comments

--- include/GPS.h
+++ include/GPS.h
@@ -1,21 +1,22 @@
 // file: header for GPS class
 #ifndef GPS_H
 #define GPS_H
-
-#include <TinyGPSPlus.h> // GEO-M8N Arduino Library
-#include <HardwareSerial.h> // For serial logging and display
+#include <TinyGPSPlus.h>
+#include <HardwareSerial.h>
+// NEO-M8N classes
+// Handles UART communication and GPS parsing
 class GPS {
 public:
-    GPS(HardwareSerial& serial);
-    void begin(uint32_t baud = 9600);
-    void update();
-    TinyGPSPlus& getGPSData();
+    GPS(HardwareSerial& serial); // Bind GPS to serial port
+    void begin(uint32_t baud = 9600); // Initialize
+    void update(); // Update from serial stream
+    TinyGPSPlus& getGPSData(); // Raw GPS data access
+    // Status and position access
     bool hasFix();
     double latitude();
     double longitude();
     uint32_t satellites();
 private:
-    HardwareSerial& _serial;
-    TinyGPSPlus _gps;
+    HardwareSerial& _serial; TinyGPSPlus _gps;
 };
 #endif
