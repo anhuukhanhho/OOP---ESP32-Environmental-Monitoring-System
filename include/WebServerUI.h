2b18ed14ee75204505754495b3daf6ac9d194365
Hồ Hữu Khánh An <106583634+anhuukhanhho@users.noreply.github.com>
2026-01-10 23:59:47 +0700

    Update WebServerUI.h comments

--- include/WebServerUI.h
+++ include/WebServerUI.h
@@ -1,25 +1,23 @@
 // file: header for WebServerUI class
 #ifndef WEB_SERVER_UI_H
 #define WEB_SERVER_UI_H
-
 #include <WebServer.h>
 #include "BME280.h"
 #include "GPS.h"
 #include "PMS7003.h"
+// Web interface classes
+// Handles HTTP routing and HTML generation
 class WebServerUI {
 public:
     WebServerUI(
-        WebServer& server,
-        BME280& bme,
-        GPS& gps,
-        PMS7003& pms
-    ); void begin();
+        WebServer& server, BME280& bme,
+        GPS& gps, PMS7003& pms
+    );
+    void begin(); // Register web routes
 private:
-    WebServer& _server;
-    BME280& _bme;
-    GPS& _gps;
-    PMS7003& _pms;
-    String generatePage();
-    void handleRoot();
+    WebServer& _server; // HTTP server instance
+    BME280& _bme; GPS& _gps; PMS7003& _pms;
+    String generatePage(); // HTML page builder
+    void handleRoot(); // Root URL handler
 };
 #endif
